0dcf7a094a0321cc16896364ae63192b9bcc5e93
Laxman Rathod <laxmanrathod.dev@gmail.com>
2026-01-10 00:06:41 +0530

    chore: configure upstash redis and add typescript settings
    - Add Upstash Redis connection string and client setup for caching
    - Add tsconfig.json to include new path aliases and extend base configuration options

---
+++ apps/api/src/config/index.ts
@@ -0,0 +1,7 @@
+import { config } from "@hoverdocs/utils";
+import { Redis } from "@upstash/redis";
+
+export const redis = new Redis({
+  url: config.UPSTASH_REDIS_REST_URL,
+  token: config.UPSTASH_REDIS_REST_TOKEN,
+});
